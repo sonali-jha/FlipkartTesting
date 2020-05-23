8b5bec564896650491d22197e4f1a8f61586f811
sonali <sonali.jha210@gmail.com>
2020-05-23 22:52:01 +0530

    First Git commit

---
+++ BRI/src/main/java/BRI/BRI/App.java
@@ -0,0 +1,13 @@
+package BRI.BRI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ BRI/src/test/java/BRI/BRI/webDriverManagerClass.java
@@ -0,0 +1,18 @@
+package BRI.BRI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class webDriverManagerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://easypymtz.commdel.com/login");
+
+	}
+
+}
